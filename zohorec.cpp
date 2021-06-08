7f5a7f7e7c727d142342c289d00d4c49ddec2788
gokulajmech <85476372+gokulajmech@users.noreply.github.com>
2021-06-08 10:50:22 +0530

    Add files via upload

---
+++ zohorec.cpp
@@ -0,0 +1,50 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int n,mid,v=0;
+	char *str=(char*)malloc(sizeof(char));
+	while((str[n]=getchar())!='\n')
+	{
+		n++;
+		str=(char*)realloc(str,n+1);
+	}
+	str[n]='\0';
+	mid=n/2;
+	for(int i=0;i<n;i++)
+	{
+		int k=n-i-1;
+		int s=mid+i;
+		for(int j=0;j<n;j++)
+		{
+			if(j>=k)
+			{
+				if(s<n)
+				{
+					for(int p=mid;p<=s;p++)
+					{
+						printf("%c",str[p]);
+						j++;
+					}
+				
+				}
+				else if(v<mid)
+				{
+					for(int p=mid;p<n;p++)
+					{
+						printf("%c",str[p]);
+						j++;
+					}v++;
+					for(int l=0;l<v;l++)
+					{
+						printf("%c",str[l]);
+						j++;
+					}
+				}
+			}
+			else
+			printf(" ");
+		}
+		printf("\n");
+	}
+}
